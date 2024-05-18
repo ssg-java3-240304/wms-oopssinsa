e09864d1868bcddd8528df82ac64ebeef6a06e14
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-18 21:31:28 +0900

    feat(dto): create LocationDto

---
+++ warehousemanager/src/main/java/com/oopssinsa/model/dto/LocationDto.java
@@ -0,0 +1,17 @@
+package com.oopssinsa.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class LocationDto {
+    private String id;
+    private char sectionId;
+    private String categoryId;
+    private int currentCapacity;
+    private int expectedCapacity;
+    private int maxCapacity;
+}
