f536e9f988d59de4b0b9a5526fc6390b78b20eeb
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-18 19:18:46 +0900

    feat(dto): create field & constructor in IbDto

--- warehousemanager/src/main/java/com/oopssinsa/model/dto/IbDto.java
+++ warehousemanager/src/main/java/com/oopssinsa/model/dto/IbDto.java
@@ -1,4 +1,18 @@
 package com.oopssinsa.model.dto;
 
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class IbDto {
+    private String id;
+    private LocalDate manufactureDate;
+    private String productId;
+    private String brandId;
+    private int quantity;
+    private LocalDate ibRequestDate;
+    private LocalDate ibDate;
+    private char status;
 }
