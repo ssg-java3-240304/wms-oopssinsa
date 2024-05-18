3ffaa598053346dd018ae1a8bd6ee0b5d164bc85
kimmame <kimmame@naver.com>
2024-05-18 23:39:57 +0900

    Feat: 창고 작업자 서비스 구현

--- warehouseworker/src/main/java/com/oopssinsa/model/dto/SubLocationDto.java
+++ warehouseworker/src/main/java/com/oopssinsa/model/dto/SubLocationDto.java
@@ -8,4 +8,34 @@ public class SubLocationDto {
     private long id;
     private int currentCapacity;
     private int sumOfSection; // 구역별 현재용량
+
+    public SubLocationDto(long id, int currentCapacity, int sumOfSection) {
+        this.id = id;
+        this.currentCapacity = currentCapacity;
+        this.sumOfSection = sumOfSection;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+
+    public int getSumOfSection() {
+        return sumOfSection;
+    }
+
+    public void setSumOfSection(int sumOfSection) {
+        this.sumOfSection = sumOfSection;
+    }
 }
--- warehouseworker/src/main/java/com/oopssinsa/model/service/IbService.java
+++ warehouseworker/src/main/java/com/oopssinsa/model/service/IbService.java
@@ -12,21 +12,47 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IbService {
+
+    /*
+    <findIb>
+    targetIb에 담겨서 전달된 입고id, 상품id, 제조일자를 통해 DB에서 동일한 입고 요청을 찾아 반환한다.
+    */
     public static IbDto findIb(IbDto targetIb) {
         SqlSession sqlSession = MyBatisTemplate.getSqlSession();
         WorkerMapper workerMapper = sqlSession.getMapper(WorkerMapper.class);
         return workerMapper.findIb(targetIb);
     }
 
+    /*
+    <getIbInstructionToDo>
+    입고 요청 테이블에서 workerId와 동일한 작업자에게 요청된 입고요청 중 현재 상태가 "R"인 요청을 모두 반환한다.
+     */
     public List<IbInstructionDto> getIbInstructionToDo(String workerId){
         SqlSession sqlSession = MyBatisTemplate.getSqlSession();
         WorkerMapper workerMapper = sqlSession.getMapper(WorkerMapper.class);
         return workerMapper.getIbInstructionToDo(workerId);
     }
 
+    /*
+    <updateIbStatus>
+    updateIb에 담겨 전달된 입고id, 상품id, 제조일자를 통해 DB에서 특정 입고 요청을 찾고 해당 입고요청을 updateIb에 담긴
+    status로 갱신한다.
+    (아마 재귀쿼리로 처리해야 할 듯 합니다.)
+     */
     public void updateIbStatus(IbDto updateIb) {
         SqlSession sqlSession = MyBatisTemplate.getSqlSession();
         WorkerMapper workerMapper = sqlSession.getMapper(WorkerMapper.class);
-        workerMapper.updateIbInstruction(updateIbInstruction);
+        workerMapper.updateIbInstruction(updateIb);
+    }
+
+    /*
+    <findIbInstruction>
+    ibInstructionDto에 담겨 전달된 입고id, 상품id, 제조일자로 특정 입고 요청을 찾아 반환한다.
+    위치별 용량 업데이트를 위해 추가했습니다.
+     */
+    public IbInstructionDto findIbInstruction(IbInstructionDto ibInstructionDto) {
+        SqlSession sqlSession = MyBatisTemplate.getSqlSession();
+        WorkerMapper workerMapper = sqlSession.getMapper(WorkerMapper.class);
+        return workerMapper.findIbInstruction(ibInstructionDto);
     }
 }
