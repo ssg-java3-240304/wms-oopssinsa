561cf3326d8cc9ad8c267c435939ab987d192810
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-19 13:19:31 +0900

    feat(warehousemanager): create 출고요청상태인 출고 조회 #29

--- warehousemanager/src/main/java/com/oopssinsa/model/dto/ObDto.java
+++ warehousemanager/src/main/java/com/oopssinsa/model/dto/ObDto.java
@@ -1,4 +1,16 @@
 package com.oopssinsa.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class ObDto {
+    private String id;
+    private String productId;
+    private String loginId;
+    private int quantity;
+    private String recipientName;
+    private String address;
+
 }
--- warehousemanager/src/main/java/com/oopssinsa/view/InputView.java
+++ warehousemanager/src/main/java/com/oopssinsa/view/InputView.java
@@ -15,6 +15,8 @@ public class InputView {
         System.out.println("2. 입고 요청 상태인 입고 조회");
         System.out.println("3. 입고 요청 상태 변경");
         System.out.println("4. 입고 작업 배정");
+        System.out.println("5. 전체 출고 처리내역 조회");
+        System.out.println("6. 출고 요청 상태인 출고 조회");
         return sc.nextInt();
     }
 
