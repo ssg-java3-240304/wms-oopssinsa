982b0b353f123e37b3fa79907d4be70d5fa17948
yeahjinjeong <yeahjin311@sookmyung.ac.kr>
2024-05-19 17:40:05 +0900

    feat: login mapper.xml #68

---
+++ warehouseworker/src/test/java/com/oopssinsa/model/dao/LoginMapperTest.java
@@ -0,0 +1,37 @@
+package com.oopssinsa.model.dao;
+
+import com.oopssinsa.model.dto.LoginDto;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.oopssinsa.common.MyBatisTemplate.getSqlSession;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginMapperTest {
+
+    SqlSession sqlSession;
+    LoginMapper loginMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.sqlSession = getSqlSession();
+        this.loginMapper = sqlSession.getMapper(LoginMapper.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.sqlSession.rollback();
+        this.sqlSession.close();
+    }
+
+    @Disabled
+    @Test
+    void test1() {
+        LoginDto loginDto = loginMapper.loginValidation("user01", "password123");
+        System.out.println(loginDto);
+    }
+}
