335a7cf2e1be845233ec33b7f4445ef4f6fea7a0
seongyeongit <wjd12118@nate.com>
2024-05-20 02:01:36 +0900

    Feat; WM쪽 DB 진행중.. 제대로 안돌아감..

--- warehousemanager/src/main/java/com/oopssinsa/model/dao/IbMapper.java
+++ warehousemanager/src/main/java/com/oopssinsa/model/dao/IbMapper.java
@@ -1,4 +1,18 @@
 package com.oopssinsa.model.dao;
 
+import com.oopssinsa.model.dto.*;
+import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
+
+@Mapper
 public interface IbMapper {
+    List<IbDto> findAllIb();
+    List<IbDto> findIbByState(char status);
+    List<IbRequestAndLocationDto> findIbRequestAndLocation();
+    int updateIbState(IbDto ibDto);
+    int updateExpectedCapacityLocation(LocationDto locationDto);
+    int updateExpectedCapacitySection(SectionDto sectionDto);
+    LocationDto findLocationByCategoryIdAndSectionId(String categoryId, char sectionId);
+    ProductDto findProductByProductId(String productId);
+    SectionDto findSectionByBrandId(String brandId);
 }
