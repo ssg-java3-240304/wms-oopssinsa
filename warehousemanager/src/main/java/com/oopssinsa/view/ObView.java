427899681240dd38ed6283cde867b43dd381ad96
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-20 17:18:56 +0900

    feat(warehousemanager): modify Ob table

--- warehousemanager/src/main/java/com/oopssinsa/view/ObView.java
+++ warehousemanager/src/main/java/com/oopssinsa/view/ObView.java
@@ -17,17 +17,17 @@ public class ObView {
 
     public void printObRequestState(List<ObRequestDto> obRequestDtos) {
         int count = 1;
-        System.out.println("+" + "-".repeat(76) + "+");
-        System.out.println("| 번호 |  출고ID  |   상품ID   |  발주자ID  |  수량  |      수령인      |  배송지  |");
-        System.out.println("|" + "-".repeat(76) + "|");
+        System.out.println("+" + "-".repeat(88) + "+");
+        System.out.println("| 번호 |  출고ID  |   상품ID   |  발주자ID  |  수량  |      수령인      |  배송지  | 출고요청일자 |");
+        System.out.println("|" + "-".repeat(88) + "|");
 //        System.out.println(
 //                "출고ID" + "  " + "상품ID" + "  " + "발주자ID" + "  " + "수량" + "  " + "수령인" + "  "
 //                        + "배송지");
         for (ObRequestDto obRequestDto : obRequestDtos) {
-            System.out.printf("|%5d|  %7s| %11s| %10s| %6d| %16s| %8s|\n",
+            System.out.printf("|%5d|  %7s| %11s| %10s| %6d| %16s| %8s| %10s|\n",
                     (count++), obRequestDto.getId(), obRequestDto.getProductId(),
                     obRequestDto.getLoginId(), obRequestDto.getQuantity(),
-                    obRequestDto.getRecipientName(), obRequestDto.getAddress());
+                    obRequestDto.getRecipientName(), obRequestDto.getAddress(), obRequestDto.getObDate());
 
 //            System.out.println(
 //                    (count++) + ". " + obRequestDto.getId() + "  " + obRequestDto.getProductId() + "  "
@@ -38,24 +38,24 @@ public class ObView {
 //            );
         }
 
-        System.out.println("+" + "-".repeat(76) + "+\n");
+        System.out.println("+" + "-".repeat(88) + "+\n");
     }
 
     public void printAllOb(List<ObDto> obDtos) {
         int count = 1;
-        System.out.println("+" + "-".repeat(125) + "+");
+        System.out.println("+" + "-".repeat(136) + "+");
         System.out.println("| 번호 |  출고ID  |   상품ID   |   제조일자   |  수량  | 발주자ID |      수령인      |  배송지  |"
-                + "   출고일자   | 출고처리상태 | 운송장번호 |");
-        System.out.println("|" + "-".repeat(125) + "|");
+                + " 출고요청일자 | 출고완료일자 | 출고처리상태 | 운송장번호 |");
+        System.out.println("|" + "-".repeat(136) + "|");
 //        System.out.println(
 //                "출고ID" + "  " + "상품ID" + "  " + "제조일자" + "  " + "수량" + "  "
 //                        + "발주자ID" + "  " + "수령인" + "  " + "배송지" + "  " + "출고일자" + "  " + "출고처리상태" + "  " + "운송장번호");
         for (ObDto obDto : obDtos) {
-            System.out.printf("|%5d|  %7s| %11s| %11s| %,6d| %8s| %15s| %8s| %12s| %11c| %9d|\n",
+            System.out.printf("|%5d|  %7s| %11s| %11s| %,6d| %8s| %15s| %8s|  %10s| %10s| %11c| %9d|\n",
                     (count++), obDto.getObId(), obDto.getProductId(),
                     obDto.getManufactureDate(), obDto.getQuantity(), obDto.getLoginId(),
                     obDto.getRecipientName(), obDto.getAddress(),
-                    obDto.getObDate(), obDto.getStatus(), obDto.getTrackingNumber());
+                    obDto.getObDate(), obDto.getCompletionObDate(), obDto.getStatus(), obDto.getTrackingNumber());
 
 //            System.out.println(
 //                    (count++) + ". " + obDto.getObId() + "  " + obDto.getManufactureDate() + "  "
@@ -67,26 +67,27 @@ public class ObView {
 //            );
         }
 
-        System.out.println("+" + "-".repeat(125) + "+\n");
+        System.out.println("+" + "-".repeat(136) + "+\n");
     }
 
     public void printObRequestAndStock(List<ObRequestAndStockDto> obRequestAndStockDtos) {
         int count = 1;
         System.out.println("상태를 변경할 출고요청을 선택해주세요.");
-        System.out.println("+" + "-".repeat(131) + "+");
+        System.out.println("+" + "-".repeat(144) + "+");
         System.out.println("| 번호 |  출고ID  |   상품ID   |  발주자ID  |  출고요청수량  |      수령인      |  배송지  |"
-                + " 출고상태여부 |     하위위치     | 재고수량 | 예정수량 |");
-        System.out.println("|" + "-".repeat(131) + "|");
+                + " 출고요청일자 | 출고상태여부 |     하위위치     | 재고수량 | 예정수량 |");
+        System.out.println("|" + "-".repeat(144) + "|");
 //        System.out.println(
 //                "출고ID" + "  " + "상품ID" + "  " + "발주자ID" + "  " + "출고요청수량" + "  " + "수령인" + "  "
 //                        + "배송지" + "  " + "출고상태여부" + "  " + "하위위치" + "  " + "재고수량" + "  " + "예정수량");
         for (ObRequestAndStockDto obRequestAndStockDto : obRequestAndStockDtos) {
-            System.out.printf("|%5d|  %7s| %11s| %10s| %,12d| %16s| %8s| %11c| %10s| %,8d| %,7d|\n",
+            System.out.printf("|%5d|  %7s| %11s| %10s| %,12d| %16s| %8s| %11s| %11c| %10s| %,8d| %,7d|\n",
                     (count++), obRequestAndStockDto.getId(), obRequestAndStockDto.getProductId(),
                     obRequestAndStockDto.getLoginId(), obRequestAndStockDto.getQuantity(),
                     obRequestAndStockDto.getRecipientName(), obRequestAndStockDto.getAddress(),
-                    obRequestAndStockDto.getObStatus(), obRequestAndStockDto.getSubLocationId(),
-                    obRequestAndStockDto.getStockQuantity(), obRequestAndStockDto.getExpectedStockQuantity());
+                    obRequestAndStockDto.getObDate(),obRequestAndStockDto.getObStatus(),
+                    obRequestAndStockDto.getSubLocationId(), obRequestAndStockDto.getStockQuantity(),
+                    obRequestAndStockDto.getExpectedStockQuantity());
 //            System.out.println(
 //                    (count++) + ". " + obRequestAndStockDto.getId() + "  " + obRequestAndStockDto.getProductId() + "  "
 //                            + obRequestAndStockDto.getLoginId()
@@ -98,25 +99,25 @@ public class ObView {
 //            );
         }
 
-        System.out.println("|" + "-".repeat(131) + "|");
+        System.out.println("|" + "-".repeat(144) + "|");
     }
 
     public void printObDetail(List<ObDetailDto> obDetailDtos) {
         int count = 1;
         System.out.println("출고 대기 상태 목록");
-        System.out.println("+" + "-".repeat(91) + "+");
-        System.out.println("| 번호 |  출고ID  |   상품ID   |   제조일자   |  수량  | 출고처리상태 |     출고일자     |"
-                + " 운송장번호 |");
-        System.out.println("|" + "-".repeat(91) + "|");
+        System.out.println("+" + "-".repeat(99) + "+");
+        System.out.println("| 번호 |  출고ID  |   상품ID   |   제조일자   |  수량  | 출고처리상태 | 출고요청일자 |" +
+                " 출고완료일자 | 운송장번호 |");
+        System.out.println("|" + "-".repeat(99) + "|");
 
 //        System.out.println(
 //                "출고ID" + "  " + "상품ID" + "  " + "제조일자" + "  " + "수량" + "  " + "출고처리상태" + "  "
 //                        + "출고일자" + "  " + "운송장번호");
         for (ObDetailDto obDetailDto : obDetailDtos) {
-            System.out.printf("|%5d|  %7s| %11s| %11s| %,6d| %11c| %16s| %9d|\n",
+            System.out.printf("|%5d|  %7s| %11s| %11s| %,6d| %11c| %11s| %11s| %9d|\n",
                     (count++), obDetailDto.getObId(), obDetailDto.getProductId(),
                     obDetailDto.getManufactureDate(), obDetailDto.getQuantity(), obDetailDto.getStatus(),
-                    obDetailDto.getObDate(), obDetailDto.getTrackingNumber());
+                    obDetailDto.getObDate(), obDetailDto.getCompletionDate(), obDetailDto.getTrackingNumber());
 //            System.out.println(
 //                    (count++) + ". " + obDetailDto.getObId() + "  " + obDetailDto.getProductId() + "  "
 //                            + obDetailDto.getManufactureDate()
@@ -126,7 +127,7 @@ public class ObView {
 //            );
         }
 
-        System.out.println("+" + "-".repeat(91) + "+\n");
+        System.out.println("+" + "-".repeat(99) + "+\n");
     }
 
     public void printImpossibleOb() {
