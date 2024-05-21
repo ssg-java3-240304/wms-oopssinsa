cc10f2f1e1b2052333b1d19a940d8d44d77b0b85
i-Veni-Vidi-Vici <biuld1234@gmail.com>
2024-05-21 22:48:37 +0900

    fix(warehousemanager): fix ib-db error

--- warehousemanager/src/main/java/com/oopssinsa/model/service/IbService.java
+++ warehousemanager/src/main/java/com/oopssinsa/model/service/IbService.java
@@ -21,7 +21,7 @@ public class IbService {
         } catch (Exception e) {
             sqlSession.rollback();
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             sqlSession.close();
         }
     }
@@ -36,7 +36,7 @@ public class IbService {
         } catch (Exception e) {
             sqlSession.rollback();
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             sqlSession.close();
         }
     }
@@ -51,7 +51,7 @@ public class IbService {
         } catch (Exception e) {
             sqlSession.rollback();
             throw new RuntimeException(e);
-        }finally {
+        } finally {
             sqlSession.close();
         }
     }
@@ -80,42 +80,25 @@ public class IbService {
         return ibMapper.findAllIb();
     }
 
-    public List<IbDto> findIbByRequestState(){
+    public List<IbDto> findIbByRequestState() {
         SqlSession sqlSession = MyBatisTemplate.getSqlSession();
         IbMapper ibMapper = sqlSession.getMapper(IbMapper.class);
         return ibMapper.findIbByRequestState();
     }
 
-    public List<IbDto> findIbByWaitingState(){
+    public List<IbDto> findIbByWaitingState() {
         SqlSession sqlSession = MyBatisTemplate.getSqlSession();
         IbMapper ibMapper = sqlSession.getMapper(IbMapper.class);
         return ibMapper.findIbByWaitingState();
     }
 
-
     public List<IbRequestAndLocationDto> findIbRequestAndLocation(List<IbDto> ibDtos) {
-        try (SqlSession sqlSession = MyBatisTemplate.getSqlSession()) {
+        try {
+            SqlSession sqlSession = MyBatisTemplate.getSqlSession();
             IbMapper ibMapper = sqlSession.getMapper(IbMapper.class);
             return ibMapper.findIbRequestAndLocation(ibDtos);
+        } catch (Exception e) {
+            throw new RuntimeException();
         }
     }
-
-
-
-//    // 진행중 쿼리 테스트중
-//    // 입고리스트에서 상품id-> 브랜드 id -> 구역 id ->  + 상품id-> 카테고리 id
-//    public List<LocationDto> findLocationsByIbDtos(List<IbDto> ibDtos) {
-//        List<LocationDto> locationDtos = new ArrayList<>();
-//        locationDtos.add(new LocationDto("location_id1", 'A', "category_id1",
-//                3, 0, 10));
-//        locationDtos.add(new LocationDto("location_id2", 'B', "category_id1",
-//                3, 0, 13));
-//        return locationDtos;
-//    }
-
-    // findLocationsByIbDtos() -> findIbRequestAndLocation 로변경
-    // ibDtos를 받으면 상품id-> 브랜드 id -> 구역 id ->  + 상품id-> 카테고리 id 을 통해서 각 입고1개당 구역을 연결시켜줘서 반환해야 함
-    // 입고 가능상태도 계산하여 행에 추가해줘야함
-    // 상품 리스트 인자로 주면 하위 위치에 ........ 하위위치 상품하나당 하위위치하나
-
 }
